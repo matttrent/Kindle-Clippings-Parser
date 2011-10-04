adb46a1eb05bc12bc124c536cb3c4d252b1dff14
Matthew Trentacoste <mt@matttrent.com>
2011-10-03 17:11:47 -0700

    Changes to service to run on my machine

--- OSX/kindle_sync.sh
+++ OSX/kindle_sync.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 if [ -d /Volumes/Kindle/documents ]
 then
-    /bin/date >> /tmp/kindle_date
-    cp /Volumes/Kindle/documents/My\ Clippings.txt /tmp
+    # /bin/date >> /tmp/kindle_date
+    cp /Volumes/Kindle/documents/My\ Clippings.txt $HOME/Archive/Filing/kindle
+    cd $HOME/Archive/Filing/kindle
+    git commit -am "Updating clippings"
 fi
 
